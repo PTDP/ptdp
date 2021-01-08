81ac7468fd726e8fe61a37bbcbbe24104b8d5c3f
Hayden Betts <haydenbetts@gmail.com>
2021-01-08 18:47:58 -0500

    Prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
-import app from './api/app';
+import * as functions from "firebase-functions";
+import app from "./api/app";
 
 export const api = functions.https.onRequest(app);
