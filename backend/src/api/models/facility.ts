dbf0e2695ee49cd7ebc52fa6c3daf7f0e01183bf
Hayden Betts <haydenbetts@gmail.com>
2021-01-09 17:55:39 -0500

    Definite relationship map

---
+++ backend/src/api/models/facility.ts
@@ -0,0 +1,16 @@
+import { Model } from 'objection'
+import { Facility as FacilityData } from '@ptdp/lib/types';
+import { Tables } from '../../db/constants';
+
+interface Facility extends FacilityData {};
+
+// https://dev.to/tylerlwsmith/using-a-typescript-interface-to-define-model-properties-in-objection-js-1231
+class Facility extends Model {
+  id!: number
+
+  static get tableName() {
+    return Tables.facilities;
+  }
+}
+
+export default Facility;
--- backend/src/api/models/rate.ts
+++ backend/src/api/models/rate.ts
@@ -2,6 +2,11 @@ import { Model } from 'objection'
 import { Rate as RateData } from '@ptdp/lib/types';
 import { Tables } from '../../db/constants';
 
+import Service from './service'
+import Company from './company';
+import Facility from './facility';
+import Scraper from './scraper';
+
 interface Rate extends RateData {};
 
 // https://dev.to/tylerlwsmith/using-a-typescript-interface-to-define-model-properties-in-objection-js-1231
@@ -15,13 +20,37 @@ class Rate extends Model {
   static get relationMappings() {
       return {
         services: {
-            relation: Model.HasOneRelation,
-            modelClass: Animal,
+            relation: Model.BelongsToOneRelation,
+            modelClass: Service,
             join: {
-                from: 'persons.fatherId',
-                to: 'persons.id'
+                from: `${Tables.rates}.service_id`,
+                to: `${Tables.services}.id`
             }
-        }
+        },
+        companies: {
+            relation: Model.BelongsToOneRelation,
+            modelClass: Company,
+            join: {
+                from: `${Tables.rates}.company_id`,
+                to: `${Tables.companies}.id`
+            }
+        },
+        facilities: {
+            relation: Model.BelongsToOneRelation,
+            modelClass: Facility,
+            join: {
+                from: `${Tables.rates}.facility_id`,
+                to: `${Tables.facilities}.id`
+            }
+        },
+        scrapers: {
+            relation: Model.BelongsToOneRelation,
+            modelClass: Scraper,
+            join: {
+                from: `${Tables.rates}.service_id`,
+                to: `${Tables.services}.id`
+            }
+        },
       }
   }
 }
