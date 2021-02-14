bbfb7cd52140ae9dcb0ee24d3ca63d746f36fc1f
Hayden Betts <haydenbetts@gmail.com>
2021-02-13 20:33:15 -0500

    Hero

--- frontend/src/app/pages/HomePage/index.tsx
+++ frontend/src/app/pages/HomePage/index.tsx
@@ -18,7 +18,7 @@ export function HomePage() {
       <NavBar />
       {/* <PageWrapper> */}
       <Masthead />
-      <Features />
+      {/* <Features /> */}
       {/* </PageWrapper> */}
     </>
   );
