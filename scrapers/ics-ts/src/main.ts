96b556b3da4cdb8ac9432914be400c275f0ce098
Hayden Betts <haydenbetts@gmail.com>
2021-03-05 21:01:04 -0500

    update build

--- scrapers/ics-ts/src/main.ts
+++ scrapers/ics-ts/src/main.ts
@@ -319,7 +319,7 @@ Apify.main(async () => {
             const states = Object.values(input.data);
             const products: ICSProduct[] = await getProducts(page, headers);
 
-            for (let i = 0; i < 1; i++) {
+            for (let i = 0; i < states.length; i++) {
                 try {
                     const handler = new SingleStateHandler(
                         states[i],
