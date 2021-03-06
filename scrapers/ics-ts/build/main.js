d518cbe5c4d4bf47fc9e629019f1e18e34ec163d
Hayden Betts <haydenbetts@gmail.com>
2021-03-05 21:44:14 -0500

    All states

--- scrapers/ics-ts/build/main.js
+++ scrapers/ics-ts/build/main.js
@@ -201,7 +201,7 @@ Apify.main(async () => {
             const headers = await getHeaders(page);
             const states = Object.values(input.data);
             const products = await getProducts(page, headers);
-            for (let i = 2; i < 3; i++) {
+            for (let i = 0; i < states.length; i++) {
                 try {
                     const handler = new SingleStateHandler(states[i], input.uuid, page, headers, products);
                     const results = await handler.run();
