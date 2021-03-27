47cdf181adcf5d34ff10c5bdb26f83a20bca2c64
Hayden Betts <haydenbetts@gmail.com>
2021-03-27 16:02:45 -0400

    Update charts panel

--- frontend/src/app/pages/Data/Features/NationalMap/charts.tsx
+++ frontend/src/app/pages/Data/Features/NationalMap/charts.tsx
@@ -30,6 +30,7 @@ import {
 } from 'react-vis';
 import { cd } from 'shelljs';
 import { latestOutstateRate } from './deckgl-layers';
+import { group } from 'console';
 
 function formatPhoneNumber(phoneNumberString) {
   //normalize string and remove all unnecessary characters
@@ -129,7 +130,8 @@ const ElectedOfficials = () => (
 const Toggle = ({ name, options, filters, setFilters }) => {
 
   const handleClick = (e) => {
-    setFilters({ ...filters, [e.target.id.split('-')[0]]: e.target.checked });
+    console.log('e.target.id', e.target.id)
+    setFilters({ ...filters, [e.target.id.split('___')[0]]: e.target.checked });
   }
 
   return (
@@ -139,15 +141,15 @@ const Toggle = ({ name, options, filters, setFilters }) => {
           return (
             <div className="relative flex items-start">
               <div className="flex items-center h-5">
-                <input onClick={handleClick} id={`${o.id}-${o.name}`} name={o.name} checked={filters && filters[`${o.id}`] === true} type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                <input onClick={handleClick} id={`${o.id}___${o.name}`} name={o.name} checked={filters && filters[`${o.id}`] === true} type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
               </div>
               <div className="ml-3 text-xs">
-                <label htmlFor={`${o.id}-${o.name}`} className="font-medium text-gray-700">{o.label}</label>
+                <label htmlFor={`${o.id}___${o.name}`} className="font-medium text-gray-700">{o.label}</label>
               </div>
               {o.description && (
                 <div className="ml-1">
-                  <a data-tip data-for={`${o.id}-description`}><InfoIcon /> </a>
-                  <ReactTooltip multiline={true} id={`${o.id}-description`} className="w-60">
+                  <a data-tip data-for={`${o.id}___description`}><InfoIcon /> </a>
+                  <ReactTooltip multiline={true} id={`${o.id}___description`} className="w-60">
                     {o.description}
                   </ReactTooltip>
                 </div>
@@ -166,13 +168,13 @@ export default function Charts({
   // selete,
   select,
   // selectedHour,
-  selectedFacility,
+  selectedFacilities,
   setChartExpanded,
   chartExpanded
 }: {
   highlight: any,
   select: any,
-  selectedFacility: Facility,
+  selectedFacilities: Facility[],
   setChartExpanded: any,
   chartExpanded: any
 }) {
@@ -191,6 +193,35 @@ export default function Charts({
     y: number;
     number: string;
   } | null>();
+  const [selectedFacility, setSelectedFacility] = useState<Facility>();
+  const [groupedFacilities, setGroupedFacilities] = useState<Facility[]>([]);
+
+
+  const tab = "py-4 px-1 text-center border-b-2 font-medium text-xs"
+  const inActiveTab = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300";
+  const activeTab = "border-indigo-500 text-indigo-600";
+
+  const groupByHIFLD = () => {
+    const grouped: Facility[] = [];
+
+    selectedFacilities.forEach((facility: Facility) => {
+      const index = grouped.findIndex((elt: Facility) => {
+        return elt.hifldid === facility.hifldid
+      });
+
+      if (index === -1) {
+        grouped.push(facility);
+      } else {
+        facility.companyFacilitiesByCanonicalFacilityId.nodes.forEach((f) => {
+          if (!grouped[index].companyFacilitiesByCanonicalFacilityId.nodes.find(elt => elt.id === f.id)) {
+            grouped[index].companyFacilitiesByCanonicalFacilityId.nodes.push(f);
+          }
+        })
+      }
+    })
+
+    return grouped;
+  }
 
   const get = async () => {
     try {
@@ -203,11 +234,13 @@ export default function Charts({
 
   const getCompanyFacilityFilters = () => {
     const names: Record<string, boolean> = {};
-    selectedFacility.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node, i) => {
-      if (i === 0) names[node.facilityInternal] = true;
-      else {
-        names[node.facilityInternal] = false;
-      }
+    groupedFacilities.forEach(g => {
+      g.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node, i) => {
+        if (i === 0) names[node.facilityInternal] = true;
+        else {
+          names[node.facilityInternal] = true;
+        }
+      })
     })
 
     return names;
@@ -215,38 +248,48 @@ export default function Charts({
 
   const getProductFilters = () => {
     const products: Record<string, boolean> = {};
-    selectedFacility.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node, i) => {
-      node.ratesByCompanyFacilityId.nodes.forEach((node) => {
-        if (i === 0) products[node.service] = true;
-        else {
-          products[node.service] = false;
-        }
+
+    groupedFacilities.forEach(g => {
+      g.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node, i) => {
+        node.ratesByCompanyFacilityId.nodes.forEach((node) => {
+          if (i === 0) products[node.service] = true;
+          else {
+            products[node.service] = true;
+          }
+        })
       })
     })
     return products;
   }
 
+  type ExpandedRate = Rate & {
+    facilityInternal: string, company: number
+  }
+
   const createSeries = () => {
+
     const validCFs = selectedFacility
-      .companyFacilitiesByCanonicalFacilityId
+      ?.companyFacilitiesByCanonicalFacilityId
       .nodes
       .filter(f => companyFacilityFilters?.[f.facilityInternal] === true);
 
     const series = {};
 
-    let flattenedRates: Rate[] = [];
+    let flattenedRates: ExpandedRate[] = [];
 
-    validCFs.forEach((cf) => {
+    validCFs?.forEach((cf) => {
       cf.ratesByCompanyFacilityId.nodes.forEach((n) => {
         for (const elt of n.updated) {
-          const flatted = { ...n };
+          const flatted: any = { ...n };
           flatted.updated = [elt];
+          flatted.facilityInternal = cf.facilityInternal;
+          flatted.company = cf.company;
           flattenedRates.push(flatted);
         }
       })
     })
 
-    const filterOnCallType = (elt: Rate) => {
+    const filterOnCallType = (elt: ExpandedRate) => {
       if (callTypeFilters[CallType.IN_STATE] && elt.inState) return true;
       if (callTypeFilters[CallType.OUT_STATE] && !elt.inState) return true;
       return false;
@@ -261,7 +304,7 @@ export default function Charts({
     flattenedRates = flattenedRates.filter(filterOnCallType).filter(filterOnService);
 
     flattenedRates.forEach((r) => {
-      const key = `${r.inState ? CallType.IN_STATE : CallType.OUT_STATE}-${r.service}`;
+      const key = `${r.inState ? CallType.IN_STATE : CallType.OUT_STATE}-${r.service}-${r.facilityInternal}-${r.company}`;
       if (series[key]) series[key].push({
         x: new Date(r.updated[0]),
         y: fifteenMinuteRate(r),
@@ -282,31 +325,43 @@ export default function Charts({
       })
     }
 
+    console.log('series', series);
+
     setAllSeries(series)
   }
 
 
   useEffect(() => {
-    if (!selectedFacility || !selectedFacility.companyFacilitiesByCanonicalFacilityId) return;
+    if (selectedFacilities) setGroupedFacilities(groupByHIFLD());
+  }, [selectedFacilities])
+
+  useEffect(() => {
+    if (!groupedFacilities.length) return;
+    setSelectedFacility(groupedFacilities[0]);
+  }, [groupedFacilities])
+
+  useEffect(() => {
+    if (!selectedFacility) return;
+
     setCompanyFacilityFilters(getCompanyFacilityFilters());
     setProductFilters(getProductFilters());
+
   }, [selectedFacility])
 
   useEffect(() => {
-    if (!selectedFacility || !selectedFacility.companyFacilitiesByCanonicalFacilityId) return;
-
+    if (!selectedFacility) return;
     createSeries();
     setHoveredPoint(null);
-  }, [productFilters, companyFacilityFilters, selectedFacility, callTypeFilters])
+  }, [productFilters, companyFacilityFilters, groupedFacilities, callTypeFilters])
 
 
-  if (!selectedFacility || !selectedFacility.companyFacilitiesByCanonicalFacilityId) {
+  if (!groupedFacilities.length) {
     return null;
   }
 
   const max15 = () => {
     let max = 0;
-    selectedFacility.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node) => {
+    selectedFacility?.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node) => {
       node.ratesByCompanyFacilityId.nodes.forEach((r) => {
         max = Math.max(max, fifteenMinuteRate(r));
       })
@@ -315,7 +370,8 @@ export default function Charts({
   }
 
   const rates: Rate[] = []
-  selectedFacility.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node) => {
+
+  selectedFacility?.companyFacilitiesByCanonicalFacilityId.nodes.forEach((node) => {
     node.ratesByCompanyFacilityId.nodes.forEach((r) => {
       rates.push(r);
     })
@@ -323,7 +379,7 @@ export default function Charts({
 
   const companies: FilterCompanies[] = [];
 
-  selectedFacility.companyFacilitiesByCanonicalFacilityId.nodes.forEach((c) => {
+  selectedFacility?.companyFacilitiesByCanonicalFacilityId.nodes.forEach((c) => {
     if (!companies.includes(c.company)) companies.push(c.company)
   })
 
@@ -333,7 +389,7 @@ export default function Charts({
     comp => {
       if (!products[comp]) products[comp] = [];
 
-      selectedFacility.companyFacilitiesByCanonicalFacilityId.nodes.filter(cf => cf.company === comp).forEach(
+      selectedFacility?.companyFacilitiesByCanonicalFacilityId.nodes.filter(cf => cf.company === comp).forEach(
         (cf) => { products[comp].push(cf) })
     })
   // show all services on the graph at once
@@ -372,12 +428,46 @@ export default function Charts({
     return date;
   }
 
-  if (!allSeries) return null;
+  console.log('selectedFacility', selectedFacility)
+
+  if (!allSeries || !selectedFacility) return null;
+
   return (
     <div className={charts} style={chartExpanded ? chartsStyleExpanded : chartsStyleNotExpanded} >
-      <h2 style={{ textAlign: 'center' }}>{selectedFacility.hifldByHifldid.name}</h2>
-      {/* ICS Internal Names*/}
-      {/* Securus Internal Names*/}
+      <div>
+        {/* <div className="sm:hidden">
+          <label htmlFor="tabs" className="sr-only">Select a tab</label>
+          <select id="tabs" name="tabs" className="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+            <option>My Account</option>
+
+            <option>Company</option>
+
+            <option selected>Team Members</option>
+
+            <option>Billing</option>
+          </select>
+        </div> */}
+        <div className="hidden sm:block">
+          <div className="border-b border-gray-200">
+            <nav className="-mb-px flex" aria-label="Tabs">
+              {groupedFacilities.map((f, i) => {
+                return (
+                  <h2 className={`${tab} ${selectedFacility?.hifldid === f.hifldid ? activeTab : inActiveTab} ${i > 0 ? "ml-2" : ""}`} onClick={() => {
+                    setSelectedFacility(groupedFacilities.find(g => g.hifldid === f.hifldid))
+                  }}>
+                    {console.log(selectedFacility?.hifldid)}
+                    {f?.hifldByHifldid?.name}
+                  </h2>
+                )
+              })}
+              {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" --> */}
+            </nav>
+          </div>
+        </div>
+      </div>
+
+
+
       <CicularButton className="absolute top-2 right-2 " onClick={handleClick}>
         <Minimize _style={{ height: 20, width: 20 }} />
       </CicularButton>
@@ -394,9 +484,9 @@ export default function Charts({
               </div>
             </div>
             <div className="mt-2">
-              {Object.entries(products).map(([company, cf]) => {
+              {Object.entries(products).map(([company, cf], i) => {
                 return (
-                  <div>
+                  <div className={i > 0 ? "mt-2" : ""}>
                     {FilterCompanies[company]}:
                     <ul>
                       {
@@ -456,21 +546,21 @@ export default function Charts({
               <div className="flex-col items-center">
                 <div className="flex-col mt-2">
                   <div>
-                    {selectedFacility.hifldByHifldid.address}
+                    {selectedFacility?.hifldByHifldid.address}
                   </div>
                   <div>
-                    {selectedFacility.hifldByHifldid.city}{", "}
-                    {selectedFacility.hifldByHifldid.state} {" "}
-                    {selectedFacility.hifldByHifldid.zip}
+                    {selectedFacility?.hifldByHifldid.city}{", "}
+                    {selectedFacility?.hifldByHifldid.state} {" "}
+                    {selectedFacility?.hifldByHifldid.zip}
                   </div>
                 </div>
                 <div className="flex-col mt-2 justify-between">
-                  <div className="flex justify-between"><span>Population:</span> <span>{selectedFacility.hifldByHifldid.population === -999 ? 'UNKNOWN' : selectedFacility.hifldByHifldid.population}</span></div>
-                  <div className="flex justify-between"><span>Capacity:</span> <span>{selectedFacility.hifldByHifldid.capacity === -999 ? 'UNKNOWN' : selectedFacility.hifldByHifldid.capacity}</span></div>
-                  <div className="flex justify-between"><span>Secure Level:</span> <span>{selectedFacility.hifldByHifldid.securelvl}</span></div>
-                  <div className="flex justify-between"><span>Type:</span> <span>{selectedFacility.hifldByHifldid.type}</span></div>
-                  <div className="flex justify-between"><span>Details Collected:</span> <span>{selectedFacility.hifldByHifldid.sourcedate && new Date(selectedFacility.hifldByHifldid.sourcedate).toLocaleDateString('en-US')}</span></div>
-                  <div className="flex justify-between"><span>Source:</span>  {selectedFacility.hifldByHifldid.source && <a className="underline cursor-pointer" onClick={() => window.open(selectedFacility.hifldByHifldid.source)}>External Link</a>} </div>
+                  <div className="flex justify-between"><span>Population:</span> <span>{selectedFacility?.hifldByHifldid.population === -999 ? 'UNKNOWN' : selectedFacility?.hifldByHifldid.population}</span></div>
+                  <div className="flex justify-between"><span>Capacity:</span> <span>{selectedFacility?.hifldByHifldid.capacity === -999 ? 'UNKNOWN' : selectedFacility?.hifldByHifldid.capacity}</span></div>
+                  <div className="flex justify-between"><span>Secure Level:</span> <span>{selectedFacility?.hifldByHifldid.securelvl}</span></div>
+                  <div className="flex justify-between"><span>Type:</span> <span>{selectedFacility?.hifldByHifldid.type}</span></div>
+                  <div className="flex justify-between"><span>Details Collected:</span> <span>{selectedFacility?.hifldByHifldid.sourcedate && new Date(selectedFacility?.hifldByHifldid.sourcedate).toLocaleDateString('en-US')}</span></div>
+                  <div className="flex justify-between"><span>Source:</span>  {selectedFacility?.hifldByHifldid.source && <a className="underline cursor-pointer" onClick={() => window.open(selectedFacility?.hifldByHifldid.source)}>External Link</a>} </div>
                 </div>
               </div>
             </div>
@@ -564,8 +654,8 @@ export default function Charts({
         </div>
       </div>
       <style>
-        {`.rv-xy-plot__series rv-xy-plot__series--linemark {
-          fill: none;
+        {`.rv-xy-plot__series.rv-xy-plot__series--linemark {
+          fill: none !important;
           }
           
           .rv-xy-canvas {
