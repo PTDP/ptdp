549f8c9faa0eb7edcbae37e6bd70d19bdd3f1b59
Hayden Betts <haydenbetts@gmail.com>
2021-04-03 19:06:46 -0400

    Add call facility local time

--- scrapers/gtl-ts/src/gtlRequester.ts
+++ scrapers/gtl-ts/src/gtlRequester.ts
@@ -273,6 +273,7 @@ export class GTLRequester {
             liveAgentFee: null,
             automatedPaymentFee: null,
             paperBillStatementFee: null,
+            callFacilityLocalTime: `${this._hour}:${this._minute} ${this._amPm}`,
         };
         try {
             const $ = cheerio.load(html);
