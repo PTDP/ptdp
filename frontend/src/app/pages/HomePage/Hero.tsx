bd51568f42654ae6063a89cd6307bd8697788d6d
JoLynne W. Martinez <jolynnemartinez@gmail.com>
2021-05-06 11:04:24 -0500

    Edited color contrast button background colors to meet accessibility guidelines

--- frontend/src/app/pages/HomePage/Hero.tsx
+++ frontend/src/app/pages/HomePage/Hero.tsx
@@ -43,7 +43,7 @@ export default () => {
           </p>
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div className="rounded-md shadow">
-                                    <a onClick={handleClick} href="/data" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                                    <a onClick={handleClick} href="/data" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-700 hover:bg-green-800 md:py-4 md:text-lg md:px-10">
                                         Explore the data
               </a>
                                 </div>
